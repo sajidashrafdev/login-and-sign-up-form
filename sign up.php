e681f575cb2ff407b2bba6b19cdf0e1fb4cb2e77
Sajid Ashraf <sajidashraf.dev@gmail.com>
2025-08-10 21:58:04 +0500

    Update sign up.php

--- sign up.php
+++ sign up.php
@@ -1,1 +1,21 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    <h1>Please Registers  Your New Account</h1>
+    <form action="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <button type="submit">Login</button>
+        <p>Don't have an account? <a href="signup.php">Sign up here</a>.</p>
+    </form>
+</body>
+</html>
